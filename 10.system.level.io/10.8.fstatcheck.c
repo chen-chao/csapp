e44519358c9a2d8d27ffb8f40255b1135a1a6188
chen-chao <wenbushi@gmail.com>
2019-07-04 23:23:11 +0800

    10.8 fstatcheck

---
+++ 10.system.level.io/10.8.fstatcheck.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+void unix_error(char *msg) {
+  fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+  exit(0);
+}
+
+int Fstat(int fd, struct stat *buf) {
+  int ret;
+  if ((ret = fstat(fd, buf)) != 0) {
+    unix_error("fstat error");
+  }
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  struct stat status;
+  char *type, *readok;
+  int fd;
+
+  if (argc != 2) {
+    printf("Usage: statcheck fd\n");
+    return -1;
+  }
+
+  char *rem;
+  fd = (int) strtol(argv[1], &rem, 10);
+  if (*rem != '\0') {
+    printf("invalid input: %s", rem);
+    return -1;
+  }
+
+  Fstat(fd, &status);
+
+  if (S_ISREG(status.st_mode)) {
+    type = "regular";
+  } else if (S_ISDIR(status.st_mode)) {
+    type = "directory";
+  } else {
+    type = "other";
+  }
+
+  if ((status.st_mode & S_IRUSR)) {
+    readok = "yes";
+  } else {
+    readok = "no";
+  }
+
+  printf("type: %s, read: %s\n", type, readok);
+
+  return 0;
+}
