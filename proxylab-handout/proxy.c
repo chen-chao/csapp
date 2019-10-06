2f3f303f33bfc89cab56f6d650b66369ad8c3836
chen-chao <wenbushi@gmail.com>
2019-10-06 23:49:47 +0800

    proxylab: sequencial http proxy server

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -1,35 +1,18 @@
 #include "csapp.h"
-#include "proxy.h"
+
+/* Recommended max cache and object sizes */
+#define MAX_CACHE_SIZE 1049000
+#define MAX_OBJECT_SIZE 102400
+
+void proxy(int fd);
+void signale_handler(int signum);
+void copy(int dst, int src, size_t chunksize);
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
+void parse_url(char *url, char *hostname, char *port, char *uri);
+void read_requesthdrs(rio_t *rp, char *buf);
 
 /* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
-
-
-int main(int argc, char *argv[])
-{
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-    exit(1);
-  }
-
-  int listenfd = Open_listenfd(argv[1]);
-
-  int connfd;
-  struct sockaddr_storage clientaddr;
-  socklen_t clientlen;
-  char hostname[MAXLINE], port[MAXLINE];
-
-  while (1) {
-    clientlen = sizeof(clientaddr);
-    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
-    printf("Accepted connection from %s\n", hostname);
-    proxy(connfd);
-    Close(connfd);
-  }
-
-  return 0;
-}
+static const char *user_agent_hdr = "Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3";
 
 void proxy(int fd)
 {
@@ -45,73 +28,118 @@ void proxy(int fd)
 
     char method[MAXLINE], url[MAXLINE], version[MAXLINE];
     sscanf(req, "%s %s %s", method, url, version);
+    printf("req: %s\n", req);
 
-    /* TODO: support other method */
-    if (strcmp(method, "GET") != 0) {
-      clienterror(fd, method, "501", "Not implemented", "proxy does not implement this method");
+    char header[MAXLINE], resp[MAXLINE];
+    /* read_requesthdrs(&rio, header); */
+    /* printf("header:\n%s", header); */
+
+    char hostname[MAXLINE], port[MAXLINE], uri[MAXLINE];
+    int connfd;
+    parse_url(url, hostname, port, uri);
+    connfd = open_clientfd(hostname, port);
+
+    if (connfd < 0) {
+      printf("Failed connection to target host\n");
+      sprintf(resp, "%s 404 FAILED\r\n", version);
+    } else {
+      printf("Connection established to target host\n");
+      sprintf(resp, "%s 200 OK\r\n", version);
     }
 
-    char hostname[MAXLINE], args[MAXLINE], port[MAXLINE];
-    parse_url(url, hostname, port);
+    Rio_writen(fd, resp, strlen(resp));
 
-    char header[MAXLINE];
     read_requesthdrs(&rio, header);
 
-    int connfd;
-    if ((connfd = open_clientfd(hostname, port)) < 0) {
-      clienterror(fd, method, "500", "Failed Connection", "Cannot connect to target host");
-    }
+    printf("client --> server\n");
+    sprintf(req, "GET %s %s\r\n", uri, version);
+    printf("req:\n%s", req);
     Rio_writen(connfd, req, strlen(req));
+    sprintf(header, "%s\r\n", header);
+    printf("header:\n%s-------------\n", header);
     Rio_writen(connfd, header, strlen(header));
 
-    /* read from connfd, write to fd */
-    off_t sendlen;
-
-    if (sendfile(connfd, fd, 0, &sendlen, NULL, 0) != 0) {
-      unix_error("sendfile failed");
-    }
+    printf("server --> client\n");
+    copy(fd, connfd, 4096);
+    Close(connfd);
 }
 
-void parse_url(char *url, char *hostname, char *port)
+void signale_handler(int signum) {
+
+}
+
+void copy(int dst, int src, size_t chunksize)
 {
-  char buf[MAXLINE];
+  char *buf = Malloc((chunksize+1)*sizeof(char));
+  ssize_t count;
+  do {
+    count = Rio_readn(src, buf, chunksize);
+    *(buf+count) = '\0';
+    printf("read from src %d: %zd\n%s\n------------\n", src, count, buf);
+
+    Rio_writen(dst, buf, count);
+    printf("sended to dst %d\n", dst);
+  } while (count == (ssize_t)chunksize);
+}
+
+void parse_url(char *url, char *hostname, char *port, char *uri)
+{
   char *p;
-  for (p = buf; *url != ':' && *url != '\0'; url++, p++) {
-    *p = *url;
-  }
+  for (p = hostname; *p = *url, *p != ':' && *p != '/' && *p != '\0'; url++, p++)
+    ;
 
   // only hostname
-  if (*url == '\0') {
-    *p = '\0';
-    strcpy(hostname, p);
+  if (*p == '\0') {
+    // defaults to http
     strcpy(port, "80");
     return;
   }
 
-  // scheme
-  if (*++url == '/') {
-    if (*++url != '/') {
-      unix_error("invalid scheme");
-    }
+  char scheme[MAXLINE];
+
+  if (*p == ':' && *(url + 1) == '/' && *(url + 2) == '/') {
+    // scheme
+    *p = '\0';
+    strcpy(scheme, hostname);
+    url += 3;
+
+    // hostname
+    for (p = hostname; *p = *url, *p != ':' && *p != '/' && *p != '\0'; url++, p++)
+      ;
   }
 
-  // hostname
-  for (p = buf; *url != ':' && *url != '/' && *url != '\0'; url++, p++) {
-    *p = *url;
-  }
-  *p = '\0';
-  strcpy(hostname, p);
-
-  // port
-  if (*url == ':') {
-    for (p = port, ++url; *url != '/' && *url != '\0'; url++) {
+  if (*p == ':') {
+    // ending hostname
+    *p = '\0';
+    // port
+    for (p = port, ++url; *url != '/' && *url != '\0'; p++, url++) {
       *p = *url;
     }
     *p = '\0';
-    strcpy(port, p);
   } else {
-    strcpy(port, "80");
+    // ending hostname
+    *p = '\0';
+
+    // port is not specified
+    if (strcmp(scheme, "https") == 0) {
+      strcpy(port, "443");
+    } else if (strcmp(scheme, "http") == 0) {
+      strcpy(port, "80");
+    } else {
+      // unknown scheme
+      strcpy(port, "");
+    }
   }
+
+
+  // uri
+  if (*url == '\0') {
+    strcpy(uri, "/");
+    return;
+  }
+
+  for (p = uri; *p = *url, *p != '\0'; p++, url++)
+    ;
 }
 
 void read_requesthdrs(rio_t *rp, char *header)
@@ -122,7 +150,6 @@ void read_requesthdrs(rio_t *rp, char *header)
   if ((ret=Rio_readlineb(rp, buf, MAXLINE)) < 0) {
     return;
   }
-  printf("%s", buf);
   while (strcmp(buf, "\r\n") != 0) {
     strcpy(header, buf);
     header += ret;
--- proxylab-handout/proxy_test.c
+++ proxylab-handout/proxy_test.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-#include "proxy.h"
 
 static int test_count = 0;
 static int test_pass = 0;
 static int main_ret = 0;
+
+void parse_url(char *url, char *hostname, char *port, char *uri);
 
 #define EXPECT_EQ_BASE(equality, expect, actual, format) \
     do {\
@@ -20,29 +21,31 @@ static int main_ret = 0;
 
 #define EXPECT_EQ_STRING(expect, actual) EXPECT_EQ_BASE(strcmp((expect), (actual)) == 0, expect, actual, "%s")
 
-static void TEST_URL(char *url, char *expect_host, char *expect_port)
+static void TEST_URL(char *url, char *expect_host,
+		     char *expect_port, char *expect_uri)
 {
-  char hostname[2000], port[2000];
-  parse_url(url, hostname, port);
+  char hostname[2000], port[2000], uri[2000];
+  parse_url(url, hostname, port, uri);
   EXPECT_EQ_STRING(expect_host, hostname);
   EXPECT_EQ_STRING(expect_port, port);
+  EXPECT_EQ_STRING(expect_uri, uri);
 }
 
 static void test_parse_url() {
-  TEST_URL("http://www.example.com", "http://www.example.com", "80");
-  TEST_URL("www.example.com", "www.example.com", "80");
-  TEST_URL("www.example.com:1234", "www.example.com", "1234");
-  TEST_URL("example.com:8000/index.html", "example.com", "8000");
-  TEST_URL("https://example.com:50037/somepage&args", "https://example.com", "50037");
+  TEST_URL("http://www.example.com", "www.example.com", "80", "/");
+  TEST_URL("www.example.com", "www.example.com", "80", "/");
+  TEST_URL("www.example.com/index.html", "www.example.com",
+	   "80", "/index.html");
+  TEST_URL("www.example.com:1234", "www.example.com", "1234", "/");
+  TEST_URL("https://example.com/index.html", "example.com",
+	   "443", "/index.html");
+  TEST_URL("https://example.com:50037/somepage&args", "example.com",
+	   "50037", "/somepage&args");
 }
 
-static void test_read_header() {
-
-}
 
 static void test_proxy() {
   test_parse_url();
-  test_read_header();
 }
 
 int main() {
